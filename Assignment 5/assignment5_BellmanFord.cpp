90b4e0b0c9743b01425e6d7f4eda55effcff5078
Mihir Rana <ranamihir@gmail.com>
2017-03-16 19:33:13 +0530

    Example + Picture of Graph

--- Assignment 5/assignment5_BellmanFord.cpp
+++ Assignment 5/assignment5_BellmanFord.cpp
@@ -23,37 +23,37 @@ int main() {
  
     graph->edge[0].source = 0;
     graph->edge[0].destination = 1;
-    graph->edge[0].weight = -1;
+    graph->edge[0].weight = 6;
 
     graph->edge[1].source = 0;
     graph->edge[1].destination = 2;
-    graph->edge[1].weight = 4;
+    graph->edge[1].weight = 3;
 
     graph->edge[2].source = 1;
     graph->edge[2].destination = 2;
-    graph->edge[2].weight = 3;
+    graph->edge[2].weight = 2;
 
-    graph->edge[3].source = 1;
-    graph->edge[3].destination = 3;
-    graph->edge[3].weight = 2;
+    graph->edge[3].source = 2;
+    graph->edge[3].destination = 1;
+    graph->edge[3].weight = 1;
 
-    graph->edge[4].source = 1;
-    graph->edge[4].destination = 4;
-    graph->edge[4].weight = 2;
+    graph->edge[4].source = 3;
+    graph->edge[4].destination = 0;
+    graph->edge[4].weight = 3;
 
-    graph->edge[5].source = 3;
-    graph->edge[5].destination = 2;
-    graph->edge[5].weight = 5;
+    graph->edge[5].source = 2;
+    graph->edge[5].destination = 3;
+    graph->edge[5].weight = 1;
 
-    graph->edge[6].source = 3;
-    graph->edge[6].destination = 1;
-    graph->edge[6].weight = 1;
+    graph->edge[6].source = 4;
+    graph->edge[6].destination = 2;
+    graph->edge[6].weight = 2;
 
     graph->edge[7].source = 4;
     graph->edge[7].destination = 3;
-    graph->edge[7].weight = -3;
+    graph->edge[7].weight = 4;
  
-    BellmanFord(graph, 0);
+    BellmanFord(graph, 4);
  
     return 0;
 }
